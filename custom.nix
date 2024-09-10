416cbcfa70d34906c31872190e0a8f3aecac979f
ankan355 <ankan355@gmail.com>
2024-09-10 06:34:23 +0000

    Savepoint-1

--- custom.nix
+++ custom.nix
@@ -2,10 +2,12 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.git-filter-repo
+    pkgs.git-filter-repo   # Git Filter Repo tool
+    pkgs.nodejs            # Node.js which includes npm
   ];
 
   shellHook = ''
-    echo "git-filter-repo has been added to your environment."
+    npm install -g firebase-tools  # Install Firebase CLI globally
+    echo "git-filter-repo and Firebase CLI have been added to your environment."
   '';
 }
